1ac2d838d1d47ba5b73c6229d154cd815c7bebe6
Hendrik <Henni.KleinH@gmail.com>
2019-06-06 15:40:16 +0200

    drop if exists

--- sqltest.py
+++ sqltest.py
@@ -6,9 +6,7 @@ import sqlite3
 connection = sqlite3.connect("dicebets.db")
 
 cursor = connection.cursor()
-
-cursor.execute("""DROP TABLE dicebets;""")
-
+cursor.execute("""DROP TABLE IF EXISTS dicebets""")
 sql_command = """
 CREATE TABLE dicebets ( 
 userId CHAR(8) PRIMARY KEY, 
@@ -17,9 +15,9 @@ amount INT);"""
 
 cursor.execute(sql_command)
 
-#sql_command = """INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
-#    VALUES (NULL, "William", "Shakespeare", "m", "1961-10-25");"""
-#cursor.execute(sql_command)
+sql_command = """INSERT INTO dicebets (userId, target, amount)
+    VALUES ("__dicebot__", 1, 100);"""
+cursor.execute(sql_command)
 
 
 #sql_command = """INSERT INTO employee (staff_number, fname, lname, gender, birth_date)
